32b7f4cb8c7d20f39fffd677a4b755a7383c7a28
thisaldil <tdimith10@gmail.com>
2025-10-07 12:05:06 +0530

    init

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running successfully!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
