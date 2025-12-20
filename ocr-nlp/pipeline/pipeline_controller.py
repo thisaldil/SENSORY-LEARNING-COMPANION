c78ce06db8f43f7085344d76997b39d41cbc7a4a
Thisal Gonsalkorala <tdimith10@gmail.com>
2025-12-20 08:19:58 +0530

    relations works simply

--- ocr-nlp/pipeline/pipeline_controller.py
+++ ocr-nlp/pipeline/pipeline_controller.py
@@ -4,6 +4,8 @@ from pipeline.nlp.clean_text import clean
 from pipeline.ocr.preprocess import preprocess_image
 from pipeline.ocr.extract import extract_text
 from pipeline.nlp.concept_extraction import extract_concepts
+from pipeline.nlp.relation_extraction import extract_relations
+
 
 MOCK_PATH = os.path.join(os.path.dirname(__file__), "../api/mock_output.json")
 
@@ -17,10 +19,12 @@ def handle_text(text):
     """
     cleaned = clean(text)
     concepts = extract_concepts(cleaned["cleaned_text"])
+    relations = extract_relations(cleaned["cleaned_text"])
 
     output = load_mock()
     output["cleaned_text"] = cleaned["cleaned_text"]
     output["concepts"] = concepts
+    output["relations"] = relations
 
     return output
 
@@ -39,10 +43,12 @@ def handle_image(image_bytes):
     
     # Extract concepts from cleaned text
     concepts = extract_concepts(cleaned["cleaned_text"])
+    relations = extract_relations(cleaned["cleaned_text"])
 
     # Prepare output
     output = load_mock()
     output["extracted_text"] = cleaned["cleaned_text"]
     output["concepts"] = concepts
+    output["relations"] = relations
 
     return output
